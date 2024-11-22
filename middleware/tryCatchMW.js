8f369d171d4a65cccca2f543a53d8cff80864792
Aguthankgod <Aguthankgod@gmail.com>
2024-11-22 16:23:21 +0100

    FIRSTCOMMIT: this wraps the routes with try and catch block ensuring an error gracifully handling

---
+++ middleware/tryCatchMW.js
@@ -0,0 +1,15 @@
+/** @typedef {import("express").RequestHandler} RequestHandler */ /**
+ * Wraps an asynchronous function in a try-catch block to handle errors.
+ *
+ * @param {function} fn: - The asynchronous function to be wrapped.
+ * @returns {function} - A new function that wraps the provided function in a try-catch block.
+ */
+module.exports = function tryCatchMW(fn) {
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
