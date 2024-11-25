d37a8dea6ddd792fb5c46527666c51d901d59e94
Aguthankgod <Aguthankgod@gmail.com>
2024-11-25 06:41:08 +0100

    FIRST-COMMIT: responsible for generating OTP codes depending the specified length.

---
+++ src/utils/OTPgen.js
@@ -0,0 +1,14 @@
+export const OTPgen = function OTPgen(length) {
+  var digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+  if (typeof length !== "number" || length <= 0) {
+    throw new Error("Invalid length. Please provide a positive number.");
+  }
+
+  let OTP = "";
+  for (let i = 0; i < length; i++) {
+    // Appending a random digit to the OTP string.
+    OTP += digits[Math.floor(Math.random() * 10)];
+  }
+  return OTP;
+};
