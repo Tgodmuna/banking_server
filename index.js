e5a44a19662c0afbf065cbf33ada46ce9c960a9d
Aguthankgod <Aguthankgod@gmail.com>
2024-11-27 02:16:04 +0100

    modification: change the how the environmental varibles are accessed. it was using config package but later o changed to use dotenv package

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
+// @ts-nocheck
+require("dotenv").config('./.env');
+const express = require("express");
+const connectDB = require("./src/DBconfig/connect");
+const config = require("config");
+const userRoutes = require("./src/routes/userRoutes");
+const profileRoutes = require("./src/routes/profileRoutes");
+const bankingRoutes = require("./src/routes/bankingRoutes");
 
-const express = require( 'express' );
-const connectDB = require( './DBconfig/connect' );
-const config = require( "config" );
 const app = express();
-
 connectDB();
+//if no secret key for  token generation is found,
+//automatically shutdown the app
 
-if ( !config.get( "jwtSecret" ) )
-{
-  console.error( "FATAL ERROR: jwtSecret is not defined." );
-  process.exit( 1 );
+if (!process.env.secretKey) {
+  console.error("FATAL ERROR: secretKey is not defined.");
+  process.exit(1);
 }
 
-app.use( express.json() );
+app.use(express.json());
+app.use("/api/user/", [userRoutes]);
+app.use("/api/profile", [profileRoutes]);
+app.use("/api/transactions", bankingRoutes);
 
+const PORT = config.get("PORT") || 3000;
 
-const PORT = process.env.PORT || 3000;
+// console.log(config.get(JWTSecret));
+// console.log(config.get(MONGO_URL));
+// console.log(config.get(PORT));
 
-app.listen( PORT, () => {
-  console.log( `Server running on port ${ PORT }` );
-} );
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
