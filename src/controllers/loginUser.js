39d5282ddab202840f75a917664a1f0b068346e2
Aguthankgod <Aguthankgod@gmail.com>
2024-11-28 17:10:37 +0100

    Modification: replace token generating implementation with a centralised utility function that generates token. - add user account details as part of response data to the client

--- src/controllers/loginUser.js
+++ src/controllers/loginUser.js
@@ -2,19 +2,24 @@ const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const _ = require("lodash");
-
+const Account = require("../models/accountModel");
+const tokenGen = require("../utils/tokenGen");
 async function authController(body) {
   const userData = _.pick(body, ["email", "password"]);
 
   // Log the input data for debugging
   console.log("Attempting to authenticate:", userData);
 
-  let user = await User.findOne({ email: userData.email });
+  let user = await User.findOne({ email: userData.email }).select('-createdAt -updatedAt -deleteAccount')
 
   if (!user) {
     console.log("User not found");
-    return null;
+    return "User not found";
   }
+
+  //get the associated user account
+  const id = user._id;
+  const accountDetails = await Account.findOne({ owner: id }).select("-createdAt -updatedAt -timestamps");
 
   const decodedPassword = await bcrypt.compare(userData.password, user.password);
 
@@ -23,13 +28,15 @@ async function authController(body) {
     return null;
   }
 
-  const token = jwt.sign(
+  const token = tokenGen(
     _.pick(user, ["name", "email", "gender", "dateOfBirth", "address", "_id"]),
-    process.env.secretKey
+    // @ts-ignore
+    process.env.secretKey,
+    "30min"
   );
 
   console.log("Authentication successful");
-  return { user, token };
+  return { user, token, accountDetails };
 }
 
 module.exports = authController;
