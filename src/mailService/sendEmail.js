82a0f742cb9381b88a7fb9aa539ce061334ff935
Aguthankgod <Aguthankgod@gmail.com>
2024-11-28 17:23:34 +0100

    this handles email sending.it takes in a message,receipient email address, and subject as an arguement

---
+++ src/mailService/sendEmail.js
@@ -0,0 +1,12 @@
+const transporter = require("./transporter");
+
+module.exports = async function sendEmail(to, subject, message) {
+  return transporter.sendMail({
+    sender: "bankMailer@gmail.com",
+    from: "tgodmuna@yahoo.com",
+    to: to,
+    subject: subject,
+    text: message,
+    priority: "high",
+  });
+};
