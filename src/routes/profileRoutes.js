e353f55930b584c6bc4fe3987a23c927c510504c
Aguthankgod <Aguthankgod@gmail.com>
2024-11-30 20:04:14 +0100

    fix: change the way user details are extracted. add protection on profile routes

--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -1,47 +1,45 @@
 // @ts-nocheck
 
 const router = require("express").Router();
+const express = require("express");
 const User = require("../models/userModel");
-const authMW = require("../middleware/loginMW");
+const authorised = require("../middleware/authorizationMW");
 const _ = require("lodash");
+const Jwt = require("jsonwebtoken");
 const tryCatchMW = require("../middleware/tryCatchMW");
 const { error } = require("console");
-require("dotenv").config();
+const errorMW = require("../middleware/errorMW");
+const uploadMW = require("../middleware/uploadMW");
 
 //view profile
 router.get(
   "/view-me",
-  authMW,
+  authorised,
   tryCatchMW(async (req, res) => {
-    const user = _.pick(req.User, [
-      "name",
-      "email",
-      "password",
-      "dateOfBirth",
-      "gender",
-      "phoneNumber",
-      "address",
-      "isVerified",
-      "profileImage",
-    ]);
+    const user = Jwt.decode(req.header("x-auth-token"));
+
+    if (!user) return res.status(500).send("internal server error");
 
     return res.status(200).json(user);
-  })
+  }),
+  errorMW
 );
 
 //update profile
 
 router.put(
   "/update",
-  authMW,
+  authorised,
   tryCatchMW(async (req, res) => {
     const { payload } = req.body;
+
     if (payload === "picture") {
       let user = await User.findByIdAndUpdate(
         req.user._Id,
         { profileImage: req.body.image },
         { new: true }
       );
+
       if (!user) throw new Error();
 
       user.save();
@@ -62,15 +60,30 @@ router.put(
       res.status(200).send("address updated successfully");
       return;
     }
-  })
+  }),
+  errorMW
 );
 
 //upload profile picture
 router.post(
   "/upload",
-  tryCatchMW(async (req, res) => {
-    //profile upload
-  })
+  authorised,
+  uploadMW,
+  tryCatchMW(async (err, req, res) => {
+    console.log("file uploaded successfully");
+
+    const filePath = `../../upload/${req.file.filename}`;
+
+    const decodedToken = Jwt.decode(req.header("x-auth-token"));
+
+    //update the user with the file path
+    const user = User.findByIdAndUpdate(user._id, { profileImage: filePath });
+
+    res.status(200).json({ message: " uploaded successfully", user });
+  }),
+  errorMW
 );
+
+router.use("/uploads", express.static("../../upload/"));
 
 module.exports = router;
