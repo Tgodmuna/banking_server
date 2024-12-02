6160bf9053307f0631ff6cf5819b261430a295b4
Aguthankgod <Aguthankgod@gmail.com>
2024-12-02 16:44:22 +0100

    add logger to the Middleware

--- src/middleware/tryCatchMW.js
+++ src/middleware/tryCatchMW.js
@@ -4,11 +4,15 @@
  * @param {function} fn: - The asynchronous function to be wrapped.
  * @returns {function} - A new function that wraps the provided function in a try-catch block.
  */
+
+const logger = require("../utils/logger");
 module.exports = function tryCatchMW(fn) {
   return async (req, res, next) => {
     try {
       await fn(req, res, next);
     } catch (error) {
+      logger.error(error);
+
       next(error);
     }
   };
